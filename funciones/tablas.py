2dc27c2f28e1357f083e458c5a480eac53ab34e2
linny2467 <167868513+linny2467@users.noreply.github.com>
2026-01-18 12:16:45 -0600

    Se editaron  las funciones "tablas y potencias", ahora son operaciones.

--- funciones/tablas.py
+++ funciones/tablas.py
@@ -1,2 +1,6 @@
 def ejecutar():
-    print("Módulo Tablas: aquí se mostrará la tabla de multiplicar seleccionada.")
+    print("=== MÓDULO TABLAS DE MULTIPLICAR ===")
+    n = int(input("Ingrese el número de la tabla que desea ver: "))
+    print("")
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
