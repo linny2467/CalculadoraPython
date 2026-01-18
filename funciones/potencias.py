ad2b87eb51d9df0439f0a894f1babf9a20931d72
linny2467 <167868513+linny2467@users.noreply.github.com>
2026-01-18 12:19:08 -0600

    Se editaron  las funciones "tablas y potencias", ahora son operaciones.

--- funciones/potencias.py
+++ funciones/potencias.py
@@ -1,2 +1,7 @@
 def ejecutar():
-    print("Módulo Potencias: aquí se calculará el cuadrado y cubo de un número.")
+    print("=== MÓDULO POTENCIAS ===")
+    n = float(input("Ingrese un número: "))
+
+    print(f"\nCuadrado: {n ** 2}")
+    print(f"Cubo: {n ** 3}")
+
